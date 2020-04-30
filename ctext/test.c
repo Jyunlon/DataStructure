b3f97eef310e219bdd5117d9c3561472eb93d9dc
Jyunlon <Jyunlon@outlook.com>
2020-04-30 15:42:08 +0800

    Update test.c

--- ctext/test.c
+++ ctext/test.c
@@ -10,19 +10,19 @@ void ctext_assign_test(){
   char *ctext;
   ctext_assign(&ctext,"Hello,Wolrd!");
   printf("%s\n",ctext);
-  ctext_clear(&ctext);
+  free(ctext);
 }
 void ctext_concat_test(){
   char *ctext;
   ctext_concat(&ctext,"Hello,","Wolrd!");
   printf("%s\n",ctext);
-  ctext_clear(&ctext);
+  free(ctext);
 }
 void ctext_intercept_test(){
   char *ctext;
   ctext_intercept(&ctext,"Hello,Wolrd!",2,5);
   printf("%s\n",ctext);
-  ctext_clear(&ctext);
+  free(ctext);
 }
 void ctext_match_bruteforce_test(){
   printf("%d\n",ctext_macth_bruteforce("Hello,World!","llo",0));
@@ -33,29 +33,29 @@ void ctext_replace_test(){
   char *ctext;
   ctext_replace(&ctext,"Hello,World!","o,W","#####");
   printf("%s\n",ctext);
-  ctext_clear(&ctext);
+  free(ctext);
 }
 void ctext_insert_test(){
   char *ctext;
   ctext_insert(&ctext,"Hello,World!","#####",2,3);
   printf("%s\n",ctext);
-  ctext_clear(&ctext);
+  free(ctext);
 }
 void ctext_remove_test(){
   char *ctext;
   ctext_remove(&ctext,"Hello,World!",2,5);
   printf("%s\n",ctext);
-  ctext_clear(&ctext);
+  free(ctext);
 }
 void ctext_reverse_test(){
   char *ctext;
   ctext_reverse(&ctext,"Hello,Wolrd!");
   printf("%s\n",ctext);
-  ctext_clear(&ctext);
+  free(ctext);
 }
 void ctext_shift_test(){
   char *ctext;
   ctext_shift(&ctext,"Hello,Wolrd!",1,3);
   printf("%s\n",ctext);
-  ctext_clear(&ctext);
+  free(ctext);
 }
