281f178972bd55361ddfc5f623ca40789c7ef581
Jyunlon <Jyunlon@outlook.com>
2020-04-30 15:08:42 +0800

    Create test.h

---
+++ ctext/test.h
@@ -0,0 +1,12 @@
+#pragma once
+
+void ctext_length_test();
+void ctext_assign_test();
+void ctext_concat_test();
+void ctext_intercept_test();
+void ctext_match_bruteforce_test();
+void ctext_replace_test();
+void ctext_insert_test();
+void ctext_remove_test();
+void ctext_reverse_test();
+void ctext_shift_test();
